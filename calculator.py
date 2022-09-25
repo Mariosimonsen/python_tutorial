a3f2147fdf7f60bb24c76e980e0ddf5b99ecc747
Mario Simonsen <mariosim93@gmail.com>
2022-09-25 20:50:54 +0200

    add sqrt fuction

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 # List of calculator functions need to be added
+import math
 
 def add(number1,number2):
     return print(number1 + number2)
@@ -10,8 +11,11 @@ def multi(number1, number2):
     return print(number1 * number2)
 
 def div(number1, number2):
-    return print(number1/number2)
+    return print(number1 / number2)
 
 def truediv(number1,number2):
     return print(number1 // number2)
+
+def sqrt(number1):
+   return print (math.sqrt(number1))
 
