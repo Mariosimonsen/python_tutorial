dce6f3a8c18d9c1c38cd98c6a5718f1d802ccd1d
Eivind Teig <eivind.teig@gmail.com>
2022-09-29 08:41:08 +0200

    mine forandringer

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-def foo():
-    return 42
+from calculator import add, sub, multi, truediv, div
 
-def bar():
-    return True
+valid_operators = ('add', 'sub', 'multi', 'truediv', 'div')
+
+while True:
+    operator = input(f'Hva vil du gjøre?\n{valid_operators}\ner dine valg: ')
+    operator = operator.lower()
+    if operator in valid_operators:
+        break
+    else:
+        print('du valgte ikke en gyldig operator, prøv igjen!\n')
+
+
+math_operators = (add, sub, multi, truediv, div)
+
+#'add' er i index 0
+
+index_of_operator = valid_operators.index(operator)
+
+while True:
+    num_1 = input('Skriv inn det første tallet: ')
+    if not num_1.isdecimal():
+        print('ugyldig tall, prøv igjen\n\n')
+        continue
+    else:
+        num_1 = int(num_1)
+        break
+
+num_2 = 34
+my_operator = math_operators[index_of_operator]
+print(my_operator(num_1, num_2))
