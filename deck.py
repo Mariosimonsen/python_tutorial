5095727007d057786fae674ac9c94f1dc5d5d002
Mario Simonsen <mariosim93@gmail.com>
2022-11-15 19:32:06 +0100

    Added fuction Draw_Random

---
+++ deck.py
@@ -0,0 +1,11 @@
+import random
+SUITS = ["D", "S", "H", "C"]
+RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+
+def Draw_Random():
+    suite = random.choice(SUITS)
+    rank = random.choice(RANKS)
+    print(F'{rank}{suite}')
+
+Draw_Random()    
