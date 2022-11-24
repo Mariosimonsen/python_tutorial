24e935e09c39732c205139fb299fe1722e682ad7
Mario Simonsen <mariosim93@gmail.com>
2022-11-24 21:54:24 +0100

    added class.

--- deck.py
+++ deck.py
@@ -1,4 +1,5 @@
 import random
+
 SUITS = ["D", "S", "H", "C"]
 RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 
--- strtoint.py
+++ strtoint.py
@@ -20,23 +20,38 @@ calculation('2+3')
 
 def calculation(equation):
     if '+' in equation:
-        num1, num2, = [int(num) for num in equation.split('+')]
+        num1, num2, = [float(num) for num in equation.split('+')]
         op = operators['+']
     elif '-' in equation:
-        num1, num2 = [int(num) for num in equation.split('-')]
+        num1, num2 = [float(num) for num in equation.split('-')]
         op = operators['-']
     elif '**' in equation:
-        num1, num2 = [int(num) for num in equation.split('**')]
+        num1, num2 = [float(num) for num in equation.split('**')]
         op = operators['**']
     elif '*' in equation:
-        num1, num2 = [int(num) for num in equation.split('*')]
+        num1, num2 = [float(num) for num in equation.split('*')]
         op = operators['*']
     elif '/' in equation:
-        num1, num2 = [int(num) for num in equation.split('/')]
+        num1, num2 = [float(num) for num in equation.split('/')]
         op = operators['/']
     elif '%' in equation:
-        num1, num2 = [int(num) for num in equation.split('%')]
+        num1, num2 = [float(num) for num in equation.split('%')]
         op = operators['%']
     return op(num1, num2)
    
 print(calculation(str(calculation('2+2')) + '+ 2')) # forklar til Eivind hvorfor dette fungerer
+
+# Divide and conquer
+'2 + 2'
+verdi = calculation('2 + 2')
+
+'4 + 2 + 3 - 7 / 3'
+part1 = calculation('4 + 2')
+part2 = calculation(f'{part1} + 3')
+part3 = calculation(f'{part2} - 7')
+part4 = calculation(f'{part3} / 3')
+
+total = calculation(str(calculation('4 + 2') + ' + 3'))
+
+#Etter beste evene lage en kortstokk, la det være strings, kan lagres i en liste, mulighet og skrive alle verdiene i en listen 2s,3s osv
+rank, suit = '2345678910jqka', 'h'
